74bc8879d02650745d18abf73b2e9dc08b451594
ali <kasraei.ar@gmail.com>
2025-05-26 00:52:58 +0200

    change implementation to use only one parameter

--- src/main/java/net/devk/regression/DataPoints.java
+++ src/main/java/net/devk/regression/DataPoints.java
@@ -7,6 +7,7 @@ import ai.djl.ndarray.types.Shape;
 
 public class DataPoints {
 
+
     private final NDArray x, y;
 
     public DataPoints(NDArray x, NDArray y) {
@@ -25,20 +26,11 @@ public class DataPoints {
     // y = Xw +bias + noise
     public static DataPoints syntheticData(NDManager manager, float[] weights, float bias, int numExamples) {
         NDArray x = manager.randomNormal(new Shape(numExamples, weights.length));
-        System.out.printf("x (independent variable), generated with random normal distribution for %d examples " +
-                "and %d features.%n", numExamples, weights.length);
         NDArray w = manager.create(weights);
-        System.out.println("w: " + w);
-        System.out.println("x: " + x);
-        //apply on a linear regression
         NDArray y = x.dot(w).add(bias);
-        System.out.println("y (dependent variable) is calculated based on y= w*x + bias ");
-        System.out.println("y: " + y);
         // generate some noise
         NDArray noise = manager.randomNormal(0, 0.01f, y.getShape(), DataType.FLOAT32);
-        System.out.println("noise to be added to the y: " + noise);
         y = y.add(noise);
-        System.out.println("after adding noise, y: " + y);
         return new DataPoints(x, y);
     }
 
--- src/main/java/net/devk/regression/LinearRegression.java
+++ src/main/java/net/devk/regression/LinearRegression.java
@@ -4,13 +4,14 @@ import ai.djl.engine.Engine;
 import ai.djl.ndarray.NDArray;
 import ai.djl.ndarray.NDList;
 import ai.djl.ndarray.NDManager;
-import ai.djl.ndarray.index.NDIndex;
 import ai.djl.ndarray.types.DataType;
 import ai.djl.ndarray.types.Shape;
 import ai.djl.training.GradientCollector;
 import ai.djl.training.dataset.ArrayDataset;
 import ai.djl.training.dataset.Batch;
 import ai.djl.translate.TranslateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import tech.tablesaw.api.FloatColumn;
 import tech.tablesaw.plotly.Plot;
 import tech.tablesaw.plotly.components.Axis;
@@ -19,8 +20,14 @@ import tech.tablesaw.plotly.components.Layout;
 import tech.tablesaw.plotly.traces.ScatterTrace;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.function.Supplier;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 
 public class LinearRegression {
+
+    private static final Logger logger = LoggerFactory.getLogger(LinearRegression.class);
 
 
     public static ScatterTrace createLineTrace(double min, double max, float weight, float bias) {
@@ -55,23 +62,28 @@ public class LinearRegression {
 
     public static void main(String[] args) throws TranslateException, IOException {
 
-        float[] weights = new float[]{2, -3.4f};
-        float bias = 4.2f;
-        int indexOfFeatureToPlot = 1;
-        int numberOfExamples = 1000;
+        logger.info("the data set will be created based on the already selected best weights and bias");
+        float[] weights = new float[]{2};
+        float bias = 3f;
+        logger.info("weights = {}", Arrays.toString(weights));
+        logger.info("bias = {}", bias);
+
+        int indexOfSelectedFeature = 0;
+        int numberOfGeneratedExamples = 1000;
         try (NDManager manager = NDManager.newBaseManager()) {
-            DataPoints dataPoints = DataPoints.syntheticData(manager, weights, bias, numberOfExamples);
+            logger.info("generating examples...");
+            DataPoints dataPoints = DataPoints.syntheticData(manager, weights, bias, numberOfGeneratedExamples);
             NDArray features = dataPoints.getX();
             NDArray labels = dataPoints.getY();
-            NDArray firstFeature = features.get(new NDIndex(String.format(":, %d", indexOfFeatureToPlot)));
-            System.out.println("using the first feature of the data points: " + firstFeature);
-//            float[] floatArray = firstFeature.toFloatArray();
-//            ScatterTrace scatterTrace = createScatterTrace(floatArray, labels.toFloatArray());
-//            Supplier<DoubleStream> ds = () -> IntStream.range(0, floatArray.length)
-//                    .mapToDouble(i -> floatArray[i]);
-//            ScatterTrace lineTrace = createLineTrace(ds.get().min().getAsDouble(), ds.get().max().getAsDouble(), weights[indexOfFeatureToPlot], bias);
-//
-//            plot(lineTrace, scatterTrace);
+//            NDArray selectedFeature = features.get(new NDIndex(String.format(":, %d", indexOfSelectedFeature)));
+//            logger.info("using the selected feature of the data points: {}", selectedFeature);
+            float[] floatArray = features.toFloatArray();
+            ScatterTrace scatterTrace = createScatterTrace(floatArray, labels.toFloatArray());
+            Supplier<DoubleStream> ds = () -> IntStream.range(0, floatArray.length)
+                    .mapToDouble(i -> floatArray[i]);
+            ScatterTrace lineTrace = createLineTrace(ds.get().min().getAsDouble(), ds.get().max().getAsDouble(), weights[indexOfSelectedFeature], bias);
+
+            plot(lineTrace, scatterTrace);
 
             int batchSize = 10;
 
@@ -81,66 +93,63 @@ public class LinearRegression {
                     .setSampling(batchSize, false) // set the batch size and random sampling to false
                     .build();
 
-            try (Batch batch = dataset.getData(manager).iterator().next()) {
-                // Call head() to get the first NDArray
-                NDArray X1 = batch.getData().head();
-                NDArray y1 = batch.getLabels().head();
-                System.out.println(X1);
-                System.out.println(y1);
-            }
-
             // we initialize weights by sampling random numbers from a normal distribution with mean 0 and
             // a standard deviation of  0.01, setting the bias  initialBias to  0
-            NDArray randomWeights = manager.randomNormal(0, 0.01f, new Shape(2, 1), DataType.FLOAT32);
-            System.out.println("random weights: " + randomWeights);
+            NDArray randomWeights = manager.randomNormal(0, 0.01f, new Shape(1), DataType.FLOAT32);
+            logger.info("random weights: {}", randomWeights);
             NDArray initialBias = manager.zeros(new Shape(1));
-            System.out.println("initial bias: " + initialBias);
+            logger.info("selecting zero as the initial bias {}", initialBias);
             NDList params = new NDList(randomWeights, initialBias);
 
-
             float lr = 0.03f;  // Learning Rate
+            logger.info("selecting {} as the initial learning rate, this should be a small number", initialBias);
             int numEpochs = 10;  // Number of Iterations
 
             // Attach Gradients
-            for (NDArray param : params) {
-                param.setRequiresGradient(true);
-            }
-
+            params.stream().forEach(param -> param.setRequiresGradient(true));
+            logger.info("training ...");
             for (int epoch = 0; epoch < numEpochs; epoch++) {
                 // Assuming the number of examples can be divided by the batch size, all
                 // the examples in the training dataset are used once in one epoch
                 // iteration. The features and tags of minibatch examples are given by X
                 // and y respectively.
                 for (Batch batch : dataset.getData(manager)) {
-                    NDArray X = batch.getData().head();
-                    NDArray y = batch.getLabels().head();
+                    NDArray selectedDataInBatch = batch.getData().head();
+                    NDArray selectedLabelsInBatch = batch.getLabels().head();
+//                    logger.info("selected data in batch: {}", selectedDataInBatch);
+//                    logger.info("selected labels in batch: {}", selectedLabelsInBatch);
 
                     try (GradientCollector gc = Engine.getInstance().newGradientCollector()) {
-                        // Minibatch loss in X and y
-                        NDArray l = squaredLoss(linreg(X, params.get(0), params.get(1)), y);
-                        gc.backward(l);  // Compute gradient on l with respect to estimatedWeight and initialBias
+                        // Minibatch loss in selectedDataInBatch and selectedLabelsInBatch
+                        NDArray linreg = linearRegression(selectedDataInBatch, params.get(0), params.get(1));
+                        NDArray squaredLoss = squaredLoss(linreg, selectedLabelsInBatch);
+//                        logger.info("squaredLoss: {} for the linear regression: {} for the params {} {} and the selected data in batch {}"
+//                                , squaredLoss, linreg, params.get(0), params.get(1), selectedDataInBatch);
+                        gc.backward(squaredLoss);  // Compute gradient on squaredLoss with respect to estimatedWeight and initialBias
                     }
                     sgd(params, lr, batchSize);  // Update parameters using their gradient
 
                     batch.close();
                 }
-                NDArray trainL = squaredLoss(linreg(features, params.get(0), params.get(1)), labels);
-                System.out.printf("epoch %d, loss %f\n", epoch + 1, trainL.mean().getFloat());
-
+                NDArray trainL = squaredLoss(linearRegression(features, params.get(0), params.get(1)), labels);
+                logger.info("epoch {}, loss {}", epoch + 1, trainL.mean().getFloat());
             }
 
-            NDArray trueW = manager.create(new float[]{2, -3.4f});
-            float trueB = 4.2f;
-
-            float[] estimatedWeight = trueW.sub(params.get(0).reshape(trueW.getShape())).toFloatArray();
-            System.out.println(String.format("Error in estimating estimatedWeight: [%f, %f]", estimatedWeight[0], estimatedWeight[1]));
-            System.out.println(String.format("Error in estimating initialBias: %f", trueB - params.get(1).getFloat()));
+            NDArray trueW = manager.create(weights);
+//            float trueB = 4.2f;
+            NDArray estimatedWeight = params.get(0).reshape(trueW.getShape());
+            float estimatedBias = params.get(1).getFloat();
+            logger.info("estimated weight: {}", estimatedWeight);
+            logger.info("estimated bias: {}", estimatedBias);
+//            float[] estimatedWeight = trueW.sub(estimatedWeight).toFloatArray();
+            logger.info("Error in estimating estimatedWeight: {}", String.format("%f", trueW.sub(estimatedWeight).toFloatArray()[0]));
+            logger.info("Error in estimating initialBias: {}", String.format("%f", bias - estimatedBias));
 
 
         }
     }
 
-    public static NDArray linreg(NDArray X, NDArray w, NDArray b) {
+    public static NDArray linearRegression(NDArray X, NDArray w, NDArray b) {
         return X.dot(w).add(b);
     }
 
